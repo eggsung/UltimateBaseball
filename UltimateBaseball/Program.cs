7d10757bafdabd094c600dd189b7156b2317866f
성유진 <akjd882@gmail.com>
2018-07-20 09:27:53 +0900

    final

--- UltimateBaseball/Program.cs
+++ UltimateBaseball/Program.cs
@@ -17,62 +17,73 @@ namespace UltimateBaseball
 			Console.WriteLine("| 숫자가 틀리면 아웃입니다.\t\t\t\t|");
 			Console.WriteLine("+-------------------------------------------------------+");
 
-			Console.WriteLine("> 수비수가 고른 숫자");
-			int number1 = 3;
-			int number2 = 1;
-			int number3 = 9;
-			Console.WriteLine(number1);
-			Console.WriteLine(number2);
-			Console.WriteLine(number3);
+            Random random = new Random();
 
-			Console.WriteLine("> 첫 번째 숫자를 입력하시오.");
-			int guess1 = int.Parse(Console.ReadLine());
-			Console.WriteLine("> 두 번째 숫자를 입력하시오.");
-			int guess2 = int.Parse(Console.ReadLine());
-			Console.WriteLine("> 세 번째 숫자를 입력하시오.");
-			int guess3 = int.Parse(Console.ReadLine());
-
-			Console.WriteLine("> 공격수가 고른 숫자");
-			Console.WriteLine(guess1);
-			Console.WriteLine(guess2);
-			Console.WriteLine(guess3);
-
-            if (guess1 == guess2 || guess2 == guess3 || guess1 == guess3)
+            int[] numbers = new int[3];
+            int index = 0;
+            while (index < 3)
             {
-                Console.WriteLine("같은 숫자를 입력하면 안 됩니다.");
+                numbers[index] = random.Next(0, 10);
+
+                bool hasDuplicate = false;
+                for (int j = 0; j < index; j++)
+                {
+                    if(numbers[index] == numbers[j])
+                    {
+                        hasDuplicate = true;
+                        break;
+                    }
+                }
+                if (!hasDuplicate)
+                {
+                    index++;
+                }
             }
-            else
+
+            int[] guesses = new int[3];
+            string [] inputMessages = { "> 첫 번째 숫자를 입력하시오.", "> 두 번째 숫자를 입력하시오.", "> 세 번째 숫자를 입력하시오." };
+
+
+            while (true)
             {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine(inputMessages[i]);
+                    guesses[i] = int.Parse(Console.ReadLine());
+                }
+
+                Console.WriteLine("> 공격수가 고른 숫자");
+
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine(guesses[i]);
+                }
+
+                if (guesses[0] == guesses[1] || guesses[0] == guesses[2] || guesses[1] == guesses[2])
+                {
+                    Console.WriteLine("같은 숫자를 입력하면 안 됩니다.");
+                    continue;
+                }
+
                 int strikeCount = 0;
                 int ballCount = 0;
-                // 첫 번째 숫자 비교
-                if (guess1 == number1)
-                {
-                    strikeCount = strikeCount + 1;
-                }
-                else if (guess1 == number2 || guess1 == number3)
-                {
-                    ballCount = ballCount + 1;
-                }
 
-                // 두 번째 숫자 비교
-                if (guess2 == number2)
+                for (int i = 0; i < 3; i++)
                 {
-                    strikeCount = strikeCount + 1;
-                }
-                else if (guess2 == number1 || guess2 == number3)
-                {
-                    ballCount = ballCount + 1;
-                }
-
-                //세 번째 숫자 비교
-                if (guess3 == number3)
-                {
-                    strikeCount = strikeCount + 1;
-                }
-                else if (guess3 == number1 || guess3 == number2)
-                {
-                    ballCount = ballCount + 1;
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (guesses[i] == numbers[j])
+                        {
+                            if (i == j)
+                            {
+                                strikeCount++;
+                            }
+                            else
+                            {
+                                ballCount++;
+                            }
+                        }
+                    }
                 }
 
                 Console.WriteLine("스트라이크:  ");
@@ -81,6 +92,12 @@ namespace UltimateBaseball
                 Console.WriteLine(ballCount);
                 Console.WriteLine("아웃:  ");
                 Console.WriteLine(3 - strikeCount - ballCount);
+
+                if (strikeCount == 3)
+                {
+                    Console.WriteLine("정답입니다!");
+                    break;
+                }
             }
         }
     }
